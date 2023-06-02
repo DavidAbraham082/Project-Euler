ed0ccb160e786e463afab6ac075867c73d719538
David Abraham [SSW] <davidabraham@ssw.com.au>
2023-06-02 23:59:37 +1000

    make file helper

--- Helpers.java
+++ Helpers.java
@@ -1,13 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Helpers
 {
-  public static String[] GetFileContents(String fileName) throws FileNotFoundException
+  public static ArrayList<String> GetFileContents(String fileName) throws FileNotFoundException
   {
-    Vector<String> output = new Vector<String>();
+    ArrayList<String> output = new ArrayList<String>();
     
     Scanner scanner = new Scanner(new File(fileName));
     while (scanner.hasNextLine())
@@ -15,6 +15,6 @@ public class Helpers
       output.add(scanner.nextLine());
     }
 
-    return (String[])output.toArray();
+    return output;
   }
 }
